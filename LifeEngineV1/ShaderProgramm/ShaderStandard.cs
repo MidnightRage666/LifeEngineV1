58c1d9e35a22a9952abd02a0be0d647fc4ab0150
MidnightRage666 <jesse.ihlenburg@outlook.com>
2026-08-17 01:07:21 +0200

    CheckShaderID

--- LifeEngineV1/ShaderProgramm/ShaderStandard.cs
+++ LifeEngineV1/ShaderProgramm/ShaderStandard.cs
@@ -47,6 +47,75 @@ namespace LifeEngineV1.ShaderProgramm
             GL.AttachShader(_shaderId, _fragmentShaderId);
 
             GL.LinkProgram(_shaderId);
+
+
+            CheckShaderStatus(_shaderId, _vertexShaderId, _fragmentShaderId);
+        }
+
+        private static void CheckShaderStatus(int programId, int vertexShaderId, int fragmentShaderId, int geometryShaderId = -1, int tessControlShaderId = -1, int tessEvalShaderId = -1)
+        {
+            GL.GetProgram(programId, GetProgramParameterName.LinkStatus, out int linkStatus);
+            if (linkStatus != 1)
+            {
+                GL.GetProgram(programId, GetProgramParameterName.InfoLogLength, out int logLength);
+                if (logLength > 0)
+                {
+                    string msg = GL.GetProgramInfoLog(programId);
+                    Console.WriteLine("[ProgramLog] " + msg);
+                }
+            }
+
+
+            string vMsg = "";
+            string fMsg = "";
+            GL.GetShader(vertexShaderId, ShaderParameter.CompileStatus, out int vertexStatus);
+            GL.GetShader(fragmentShaderId, ShaderParameter.CompileStatus, out int fragmentStatus);
+            if (vertexStatus == 0 || fragmentStatus == 0)
+            {
+                if (vertexStatus == 0)
+                {
+                    vMsg = GL.GetShaderInfoLog(vertexShaderId);
+                    Console.WriteLine("[ShaderVertex] " + vMsg);
+                }
+                if (fragmentStatus == 0)
+                {
+                    fMsg = GL.GetShaderInfoLog(fragmentShaderId);
+                    Console.WriteLine("[ShaderFragment] " + fMsg);
+                }
+            }
+
+            if (geometryShaderId > 0)
+            {
+                string gMsg = "";
+                GL.GetShader(geometryShaderId, ShaderParameter.CompileStatus, out int geometryStatus);
+                if (geometryStatus == 0)
+                {
+                    gMsg = GL.GetShaderInfoLog(geometryShaderId);
+                    Console.WriteLine("[ShaderGeometry] " + gMsg);
+                }
+            }
+
+            if (tessControlShaderId > 0)
+            {
+                string gMsg = "";
+                GL.GetShader(tessControlShaderId, ShaderParameter.CompileStatus, out int tcStatus);
+                if (tcStatus == 0)
+                {
+                    gMsg = GL.GetShaderInfoLog(tessControlShaderId);
+                    Console.WriteLine("[ShaderTessC] " + gMsg);
+                }
+            }
+
+            if (tessEvalShaderId > 0)
+            {
+                string gMsg = "";
+                GL.GetShader(tessControlShaderId, ShaderParameter.CompileStatus, out int teStatus);
+                if (teStatus == 0)
+                {
+                    gMsg = GL.GetShaderInfoLog(tessEvalShaderId);
+                    Console.WriteLine("[ShaderTessE] " + gMsg);
+                }
+            }
         }
 
         public static int GetShaderId()
